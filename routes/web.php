05fdb9b4d329fb262946b20d5f0cebff8c0295ca
art-yom-xvii <artjoms.doilnicins@inbox.lv>
2025-07-13 12:37:15 +0000

    Create empty laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,7 @@
 <?php
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\CheckoutController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Home page
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-// Product routes
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
-
-// Category routes
-Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.show');
-
-// Cart routes
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'store'])->name('cart.store');
-Route::patch('/cart/items/{item}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/items/{item}', [CartController::class, 'destroy'])->name('cart.destroy');
-
-// Checkout routes
-Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
-Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
-Route::get('/checkout/confirmation/{order}', [CheckoutController::class, 'confirmation'])->name('checkout.confirmation');
-
-// Static pages
-Route::get('/about-us', [PageController::class, 'about'])->name('pages.about');
-Route::get('/contact', [PageController::class, 'contact'])->name('pages.contact');
-Route::post('/contact', [PageController::class, 'contactSubmit'])->name('pages.contact.submit');
-Route::get('/faq', [PageController::class, 'faq'])->name('pages.faq');
-Route::get('/privacy-policy', [PageController::class, 'privacy'])->name('pages.privacy');
-Route::get('/terms-of-service', [PageController::class, 'terms'])->name('pages.terms');
-Route::get('/platforms', [PageController::class, 'platforms'])->name('pages.platforms');
-
-// Authentication Routes
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', function() {
-        return view('profile');
-    })->name('profile');
+Route::get('/', function () {
+    return view('welcome');
 });
